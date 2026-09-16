076c5a5aa4386751eac595acf8a3e6b4049a6ac8
Luis-Fernando <136400011+Luis-Fernando-Developer@users.noreply.github.com>
2026-09-16 15:44:31 -0300

    Change main layout to use flex column

--- src/components/client/ClientLayout.tsx
+++ src/components/client/ClientLayout.tsx
@@ -274,7 +274,7 @@ export default function ClientLayout() {
           </div>
         </header>
 
-        <main className="flex-1 min-h-0 w-full overflow-y-auto overflow-x-hidden bg-gradient-hero">
+        <main className="flex-1 min-h-0 w-full flex flex-col overflow-y-auto overflow-x-hidden bg-gradient-hero">
           <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8 w-full space-y-8">
             <div className="flex flex-col md:flex-row md:items-center justify-between gap-4">
               <div>
